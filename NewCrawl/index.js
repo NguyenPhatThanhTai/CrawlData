5da791ab1c75199da019ae5a08e44946b373f914
NguyenPhatThanhTai <57514749+NguyenPhatThanhTai@users.noreply.github.com>
2020-05-11 16:11:59 +0700

    final

--- NewCrawl/index.js
+++ NewCrawl/index.js
@@ -1,30 +1,29 @@
 var express = require("express");
 var fs = require('fs');
 var app = express();
+var path = require('path');
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.set("views", "./views")
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
-const cron = require("node-cron");
+// const cron = require("node-cron");
 var request = require("request");
 var cheerio = require("cheerio");
 app.get("/", function(req, res) {
-    cron.schedule("* * * * *", function() {
-        console.log("running a task every minute");
 
-        request("https://cacnuoc.vn/covid-19/", function(error, response, body) {
-            if (error) {
-                console.log(error);
-            } else {
-                $ = cheerio.load(body);
-                const ds = $(body).find("tbody > tr ");
-                // var ds = $(body).find("tbody > tr > td > a");
-                const obj = {
-                    'data': []
-                }
-                ds.each(function(i, e) {
-                    obj.data.push({
+    request("https://cacnuoc.vn/covid-19/", function(error, response, body) {
+        if (error) {
+            console.log(error);
+        } else {
+            $ = cheerio.load(body);
+            const ds = $(body).find("tbody > tr");
+            // var ds = $(body).find("tbody > tr > td > a");
+            const obj = {
+                'global': [],
+            }
+            ds.each(function(i, e) {
+                    obj.global.push({
                         "Country": $(this).find("td:nth-child(1)").text(),
                         "TotalCase": $(this).find("td:nth-child(2)").text(),
                         "NewCase": $(this).find("td:nth-child(3)").text(),
@@ -38,20 +37,20 @@ app.get("/", function(req, res) {
                         "Tested": $(this).find("td:nth-child(11)").text(),
                         "Tested rate": $(this).find("td:nth-child(12)").text(),
                         "Area": $(this).find("td:nth-child(13)").text(),
-
-
                     })
                 })
+                // cron.schedule("* * * * *", function() {
+            console.log("running a task every minute");
+            const json = JSON.stringify(obj);
+            fs.writeFile('./views/datas.json', json, 'utf-8', (err) => {
+                if (err) throw err;
+            })
+            res.status(200).json({
+                status: 'success',
+                data: obj
+            });
+            // })
 
-                const json = JSON.stringify(obj);
-                fs.writeFile('datas.json', json, 'utf-8', (err) => {
-                        if (err) throw err;
-                    })
-                    // ds.each(function(i, e) {
-                    //     console.log($(this).text());
-                    // }), res.render("trangchinh", { html: body });
-            }
-        })
-
+        }
     })
 });
